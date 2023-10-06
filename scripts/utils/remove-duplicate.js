df5ad8eca5c290c92640f10f8a05383d52dca4dc
dev_cook <studiokad75@gmail.com>
2023-10-06 16:50:39 +0200

    update/remove duplicate select options

--- scripts/utils/remove-duplicate.js
+++ scripts/utils/remove-duplicate.js
@@ -1,8 +1,7 @@
 import { toCapitalize } from "./to-capitalize.js";
 
-const options = [];
-
 export const removeDuplicate = (arr) => {
+  const options = [];
   arr.forEach((option) => {
     if (options.includes(toCapitalize(option.textContent))) option.remove();
     else options.push(toCapitalize(option.textContent));
